8116cdfa4cbf47c232a67451319c867531bbd533
Nitesh150791 <109646228+Nitesh150791@users.noreply.github.com>
2026-06-26 13:25:46 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,26 @@
 
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Python Backend Running!"
+
+@app.route("/db")
+def db():
+    try:
+        conn = mysql.connector.connect(
+            host="mysql-service",
+            user="root",
+            password="root",
+            database="mydb"
+        )
+        return "✅ Connected to MySQL"
+    except:
+        return "❌ DB connection failed"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+``
